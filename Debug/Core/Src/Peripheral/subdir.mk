8967aec23804fe4d6376f91bd8cdc80cbf6d405c
xuantho.do <xuantho.do@citynow.vn>
2022-03-23 23:42:23 +0700

    change logic at sim pin

--- Debug/Core/Src/Peripheral/subdir.mk
+++ Debug/Core/Src/Peripheral/subdir.mk
@@ -33,10 +33,3 @@ C_DEPS += \
 Core/Src/Peripheral/%.o: ../Core/Src/Peripheral/%.c Core/Src/Peripheral/subdir.mk
 	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xE -c -I../Core/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
 
-clean: clean-Core-2f-Src-2f-Peripheral
-
-clean-Core-2f-Src-2f-Peripheral:
-	-$(RM) ./Core/Src/Peripheral/app_gpio.d ./Core/Src/Peripheral/app_gpio.o ./Core/Src/Peripheral/app_i2c.d ./Core/Src/Peripheral/app_i2c.o ./Core/Src/Peripheral/app_spi.d ./Core/Src/Peripheral/app_spi.o ./Core/Src/Peripheral/app_system_clock_config.d ./Core/Src/Peripheral/app_system_clock_config.o ./Core/Src/Peripheral/app_timer.d ./Core/Src/Peripheral/app_timer.o ./Core/Src/Peripheral/app_uart.d ./Core/Src/Peripheral/app_uart.o
-
-.PHONY: clean-Core-2f-Src-2f-Peripheral
-
